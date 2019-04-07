733724a62c6f8703f25766ab52ece05a5d2966e9
Филиппов Алексей <filippovalex24@mail.ru>
2019-04-07 15:58:14 +0700

    create UI classes

---
+++ VFNoteBook/VFNoteBook/UI/NoteList/View/Cell/VFNoteListTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  VFNoteListTableViewCell.h
+//  VFNoteBook
+//
+//  Created by Филиппов Алексей on 07/04/2019.
+//  Copyright © 2019 BuboBubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VFNoteListTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
