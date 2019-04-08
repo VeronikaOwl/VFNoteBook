bfc514d7fe366c33fda651d4de360fbd2d7b2d12
Филиппов Алексей <filippovalex24@mail.ru>
2019-04-08 20:28:12 +0700

    implement NSCoding protocol

--- VFNoteBook/VFNoteBook/Models/NoteModel/VFNoteModel.m
+++ VFNoteBook/VFNoteBook/Models/NoteModel/VFNoteModel.m
@@ -8,6 +8,11 @@
 
 #import "VFNoteModel.h"
 
+NSString * const cNameKey = @"name";
+NSString * const cTextKey = @"text";
+NSString * const cIdentifierKey = @"identifier";
+
+
 @implementation VFNoteModel
 
 - (instancetype) initWithName: (NSString *) name
@@ -21,6 +26,30 @@
     _name = name;
     _text = text;
     _identifier = identifier;
+  }
+  return self;
+}
+
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+  [aCoder encodeObject: self.name
+                forKey: cNameKey];
+  [aCoder encodeObject: self.text
+                forKey: cTextKey];
+  [aCoder encodeInteger: self.identifier
+                 forKey: cIdentifierKey];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+  self = [super init];
+  
+  if (self != nil)
+  {
+    _name = [aDecoder decodeObjectForKey: cNameKey];
+    _text = [aDecoder decodeObjectForKey: cTextKey];
+    _identifier = [aDecoder decodeIntegerForKey: cIdentifierKey];
   }
   return self;
 }
